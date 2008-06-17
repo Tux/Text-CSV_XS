ba9588589d76f737cb67f287d9a73558743903d8
H.Merijn Brand <merijn@nb09.(none)>
2008-06-17 12:36:09 +0200

    Add -n option to gencsv.pl to suppress fields with newlines

--- sandbox/gencsv.pl
+++ sandbox/gencsv.pl
@@ -2,6 +2,20 @@
 
 use strict;
 
+sub usage
+{
+    my $err = shift and select STDERR;
+    print "usage: $0: [-n] [cols [rows]]\n";
+    exit $err;
+    } # usage
+
+use Getopt::Long qw(:config bundling nopermute);
+my $opt_n = 0;		# Disable newlines
+GetOptions (
+    "help|?"	=> sub { usage (0); },
+    "n"		=> \$opt_n,
+    ) or usage (1);
+
 my ($cols, $rows) = @ARGV;
 $cols ||= 10;
 $rows ||= 10;
@@ -10,7 +24,7 @@ use Text::CSV_XS;
 
 our $csv = Text::CSV_XS->new ({ binary => 1, eol => "\r\n" });
 
-my @f = (
+my @f = grep { defined $_ && $opt_n ? !/\n/ : 1 } (
     1, "Text::CSV_XS", $csv->version, "H.Merijn Brand",
     "1 \x{20ac} = 8.012 NOK", undef, "", "'\n,", '"', "Y");
 
