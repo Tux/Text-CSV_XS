de7646924c51bc5a96035d95bd4c2aaff94e2237
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2014-08-08 14:32:48 +0200

    add JSON (jcsv) to CSV.pm

--- sandbox/CSV.pm
+++ sandbox/CSV.pm
@@ -1,8 +1,12 @@
 use Text::CSV_XS qw( csv );
 use Data::Peek;
+use JSON;
 
 BEGIN { *CSV:: = \%Text::CSV_XS::; }
-$VERSION = "0.02";
+$VERSION = "0.04";
+
+sub dcsv { DDumper     (csv (@_, diag_verbose => 9)); }
+sub jcsv { decode_json (csv (@_, diag_verbose => 9)); }
 
 1;
 
@@ -12,13 +16,25 @@ CSV - Alias for Text::CSV_XS
 
 =head1 SYNOPSIS
 
-  perl -MCSV -wle'csv (in => [[1,2]], out => *STDOUT)'
+  perl -MCSV -wle'csv (in => [[1,2]])'
+
   perl -MCSV -wle'DDumper (csv (in => \q{foo,"bar, foo",quux}))'
+  perl -MCSV -wle'dcsv (in => \q{foo,"bar, foo",quux})'
+
+  perl -MCSV -wle'print jcsv (in => "file.csv")'
 
 =head1 DESCRIPTION
 
-Wrapper for Text::CSV_XS with csv importing also Data::Peek
-See L<Text::CSV_XS>, L<Data::Peek>
+Wrapper for Text::CSV_XS with csv importing also Data::Peek and JSON
+See L<Text::CSV_XS>, L<Data::Peek>, L<JSON>
+
+=head2 dcsv
+
+Uses L<Data::Peek>'s DDumper to output the result of C<csv>
+
+=head2 jcsv
+
+Uses L<JSON>'s decode_json to convert the result of C<csv> to JSON.
 
 =head1 AUTHOR
 
