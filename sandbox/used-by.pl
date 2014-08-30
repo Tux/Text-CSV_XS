04cc2c24b7fb70bf880e84f7d31995d1d57f721b
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2014-08-30 12:04:34 +0200

    new used-by tests checking

--- sandbox/used-by.pl
+++ sandbox/used-by.pl
@@ -3,6 +3,13 @@
 use 5.16.2;
 use warnings;
 
+sub usage
+{
+    my $err = shift and select STDERR;
+    say "usage: $0 [--list]";
+    exit $err;
+    } # usage
+
 use Cwd;
 use LWP;
 use LWP::UserAgent;
@@ -11,7 +18,13 @@ use CPAN;
 use Capture::Tiny qw( :all );
 use Term::ANSIColor qw(:constants :constants256);
 use Test::More;
-use XML::Simple;
+
+use Getopt::Long qw(:config bundling passthrough);
+GetOptions (
+    "help|?"	=> sub { usage (0); },
+    "a|all!"	=> \my $opt_a,	# Also build for known FAIL (they might have fixed it)
+    "l|list!"	=> \my $opt_l,
+    ) or usage (1);
 
 my $tm = shift // do {
     (my $d = getcwd) =~ s{.*CPAN/([^/]+)(?:/.*)?}{$1};
@@ -24,17 +37,40 @@ my %tm = map { $_ => 1 } qw( );
 $| = 1;
 $ENV{AUTOMATED_TESTING} = 1;
 # Skip all dists that
-# - are FAIL but not due to Data::Peek
-# - that require interaction (not dealt with in distroprefs)
+# - are FAIL not due to the mudule being tested (e.g. POD or signature mismatch)
+# - that require interaction (not dealt with in distroprefs or %ENV)
 # - are not proper dists (cannot use CPAN's ->test)
 # - require external connections or special devices
-my %skip = map { $_ => 1 } @{{
-    "Data-Peek"   => [qw( GSM-Gnokii					)],
-    "DBD-CSV"     => [qw( ASNMTAP					)],
-    "Text-CSV_XS" => [qw( App-Framework CGI-Application-Framework
-                          RT-Extension-Assets-Import-CSV
-                          RT-View-ConciseSpreadsheet Text-ECSV Text-MeCab
-                          Tripletail chart dbMan hwd xDash xls2csv	)],
+my %skip = $opt_a ? () : map { $_ => 1 } @{{
+    "Data-Peek"   => [
+	"GSM-Gnokii",				# External device
+	],
+    "DBD-CSV"     => [
+	"ASNMTAP",
+	],
+    "Text-CSV_XS" => [
+ 	"App-Framework",			# Questions
+	"CGI-Application-Framework",		# Unmet prerequisites
+	"CohortExplorer",			# Unmet prerequisites
+	"Connector",				# no Makefile.PL (in Annelidous)
+	"Finance-Bank-DE-NetBank",		# Module signatures
+	"RT-Extension-Assets-Import-CSV",	# Questions
+	"RT-View-ConciseSpreadsheet",		# Questions
+#	"Text-CSV-Track",			# encoding, patch filed at RT
+	"Text-ECSV",				# POD, spelling
+	"Text-MeCab",				# Questions
+	"Text-TEI-Collate",			# Unmet prerequisites
+	"Text-Tradition",			# Unmet prerequisites
+	"Tripletail",				# Makefile.PL broken
+	"WWW-Analytics-MultiTouch",		# Unmet prerequisites
+	"Webservice-InterMine",			# Unmet prerequisites
+	"YamlTime",				# Unmet prerequisites
+	"chart",				# Questions (in Apache::Wyrd)
+	"dbMan",				# Questions
+	"hwd",					# Own tests fail
+	"xDash",				# Questions
+#	"xls2csv",
+	],
     }->{$tm} // []};
 
 my $ua  = LWP::UserAgent->new (agent => "Opera/12.15");
@@ -77,7 +113,30 @@ sub get_from_cpants
     return @h;
     } # get_from_cpants
 
-foreach my $h (get_from_cpants (), get_from_cpantesters ()) {
+sub get_from_meta
+{
+    my $url = "https://metacpan.org/requires/distribution/$tm";
+    my $rsp = $ua->request (HTTP::Request->new (GET => $url));
+    unless ($rsp->is_success) {
+	warn "meta failed: ", $rsp->status_line, "\n";
+	return;
+	}
+    my $tree = HTML::TreeBuilder->new;
+       $tree->parse_content ($rsp->content);
+    my @h;
+    foreach my $a ($tree->look_down (_tag => "a", class => "ellipsis",
+					href => qr{/release/})) {
+	(my $h = $a->attr ("href")) =~ s{.*release/}{};
+	$h =~ m{^$tm\b} and next;
+	push @h, $h;
+	}
+    return @h;
+    } # get_from_meta
+
+foreach my $h ( get_from_cpants (),
+		get_from_cpantesters (),
+		get_from_meta (),
+		) {
     exists $skip{$h} || $h =~ m{^( $tm (?: $ | / )
 				 | Task-
 				 | Bundle-
@@ -93,7 +152,14 @@ unless (keys %tm) {
     exit 0;
     }
 
+if ($opt_l) {
+    ok (1, $_) for sort keys %tm;
+    done_testing;
+    exit 0;
+    }
+
 my %rslt;
+#$ENV{AUTOMATED_TESTING} = 1;
 foreach my $m (sort keys %tm) {
     my $mod = CPAN::Shell->expand ("Module", "/$m/") or next;
     # diag $m;
