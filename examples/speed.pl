93be3f3123a3270fc720e11e4d35f4d57c8224ab
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2014-08-30 11:55:07 +0200

    increase speed test size to minimize noise

--- examples/speed.pl
+++ examples/speed.pl
@@ -13,29 +13,30 @@ use Benchmark qw(:all :hireswallclock);
 our $csv = Text::CSV_XS->new ({ eol => "\n" });
 
 my $duration = int (shift || 2);
-our @fields1 = (
+my  @fields = (
     "Wiedmann", "Jochen",
     "Am Eisteich 9",
     "72555 Metzingen",
     "Germany",
     "+49 7123 14881",
     "joe\@ispsoft,de");
-our @fields10  = (@fields1) x 10;
-our @fields100 = (@fields1) x 100;
+our @fields2   = (@fields) x 2;
+our @fields20  = (@fields) x 20;
+our @fields200 = (@fields) x 200;
 
-$csv->combine (@fields1  ); our $str1   = $csv->string;
-$csv->combine (@fields10 ); our $str10  = $csv->string;
-$csv->combine (@fields100); our $str100 = $csv->string;
+$csv->combine (@fields2  ); our $str2   = $csv->string;
+$csv->combine (@fields20 ); our $str20  = $csv->string;
+$csv->combine (@fields200); our $str200 = $csv->string;
 
 timethese (-1.5,{
 
-    "combine   1"	=> q{ $csv->combine (@fields1  ) },
-    "combine  10"	=> q{ $csv->combine (@fields10 ) },
-    "combine 100"	=> q{ $csv->combine (@fields100) },
+    "combine   2"	=> q{ $csv->combine (@fields2  ) },
+    "combine  20"	=> q{ $csv->combine (@fields20 ) },
+    "combine 200"	=> q{ $csv->combine (@fields200) },
 
-    "parse     1"	=> q{ $csv->parse   ($str1     ) },
-    "parse    10"	=> q{ $csv->parse   ($str10    ) },
-    "parse   100"	=> q{ $csv->parse   ($str100   ) },
+    "parse     2"	=> q{ $csv->parse   ($str2     ) },
+    "parse    20"	=> q{ $csv->parse   ($str20    ) },
+    "parse   200"	=> q{ $csv->parse   ($str200   ) },
 
     });
 
@@ -48,7 +49,7 @@ my $bigfile = "_file.csv";
 if ($] >= 5.010) {
     eval "use Sys::MemInfo qw( freemem );";
     unless ($@) {
-	my $need = $line_count * (1 + length $str10);
+	my $need = $line_count * (1 + length $str20);
 	if (freemem () > $need) {
 	    $mem = "";
 	    open $io, ">", \$mem;
@@ -57,17 +58,17 @@ if ($] >= 5.010) {
     }
 $io or open $io, ">", $bigfile;
 
-$csv->print ($io, \@fields10) or die "Cannot print ()\n";
-timethese ($line_count, { "print    io" => q{ $csv->print ($io, \@fields10) }});
+$csv->print ($io, \@fields20) or die "Cannot print ()\n";
+timethese ($line_count, { "print    io" => q{ $csv->print ($io, \@fields20) }});
 close   $io;
 my $l = $mem ? length ($mem) : -s $bigfile;
 $l or die "Buffer/file is empty!\n";
-my @f = @fields10;
+my @f = @fields20;
 $csv->can ("bind_columns") and $csv->bind_columns (\(@f));
 open    $io, "<", ($mem ? \$mem : $bigfile);
 timethese ($line_count, { "getline  io" => q{ my $ref = $csv->getline ($io) }});
 close   $io;
-print "Data was $l bytes long, line length ", length ($str10), "\n";
+print "Data was $l bytes long, line length ", length ($str20), "\n";
 $mem or unlink $bigfile;
 
 __END__
