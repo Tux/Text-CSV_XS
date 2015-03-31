01c6437479d332a4613c0967d9d22141a73e8d00
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2015-03-31 08:41:38 +0200

    sync: no ChangeLog date check on meta-check

--- sandbox/genMETA.pm
+++ sandbox/genMETA.pm
@@ -278,11 +278,13 @@ sub check_changelog
 	    ([0-9]{4})\b/x or next;
 	my ($d, $m, $y) = ($1 + 0, ($mnt{lc $2} || $2) + 0, $3 + 0);
 	printf STDERR "Most recent ChangeLog entry is dated %02d-%02d-%04d\n", $d, $m, $y;
-	my @t = localtime;
-	my $D = Delta_Days ($y, $m , $d, $t[5] + 1900, $t[4] + 1, $t[3]);
-	$D < 0 and die  RED,    "Last entry in $td[0] is in the future!",               RESET, "\n";
-	$D > 2 and die  RED,    "Last entry in $td[0] is not up to date ($D days ago)", RESET, "\n";
-	$D > 0 and warn YELLOW, "Last entry in $td[0] is not today",                    RESET, "\n";
+	unless ($ENV{SKIP_CHANGELOG_DATE}) {
+	    my @t = localtime;
+	    my $D = Delta_Days ($y, $m , $d, $t[5] + 1900, $t[4] + 1, $t[3]);
+	    $D < 0 and die  RED,    "Last entry in $td[0] is in the future!",               RESET, "\n";
+	    $D > 2 and die  RED,    "Last entry in $td[0] is not up to date ($D days ago)", RESET, "\n";
+	    $D > 0 and warn YELLOW, "Last entry in $td[0] is not today",                    RESET, "\n";
+	    }
 	last;
 	}
     } # check_changelog
