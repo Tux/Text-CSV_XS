42315722bc353c6aa8a6523c18d59466a171f797
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2015-11-04 08:55:58 +0100

    Add confusion option to rewrite example

--- examples/rewrite.pl
+++ examples/rewrite.pl
@@ -3,11 +3,44 @@
 use strict;
 use warnings;
 
+sub usage {
+    my $err = shift and select STDERR;
+    print "usage: $0 [--ms] [--confuse] [--invisible]\n";
+    exit $err;
+    } # usage
+
+use Getopt::Long qw(:config bundling);
+GetOptions (
+    "help|?"		=> sub { usage (0); },
+    "s|sep=s"		=> \my $in_sep,
+    "m|ms!"		=> \my $ms,
+    "c|confuse!"	=> \my $confuse,
+    "i|invisible!"	=> \my $invis,
+    ) or usage (1);
+$invis and $confuse++;
+
 use Text::CSV_XS qw( csv );
 
-my $io = shift || \*DATA;
+# U+0022 "	QUOTATION MARK			"
+# U+002c ,	COMMA				,
+# U+037e ;	GREEK QUESTION MARK		;
+# U+201a ‚	SINGLE LOW-9 QUOTATION MARK	,
+# U+2033 ″	DOUBLE PRIME			"
+# U+2063	INVISIBLE SEPARATOR
 
-csv (in => csv (in => $io, sep_char => ";"), out => \*STDOUT);
+my $io  = shift || \*DATA;
+my $eol = $ms ? "\r\n" : "\n";
+my $sep = $confuse ? $ms ? "\x{037e}" : $invis ? "\x{2063}" : "\x{201a}"
+		   : $ms ? ";" : ",";
+my $quo = $confuse ? "\x{2033}" : '"';
+
+binmode STDOUT, ":encoding(utf-8)";
+
+csv (in  => csv (in => $io, sep => $in_sep // ";"),
+     out => \*STDOUT,
+     eol => $eol,
+     sep => $sep,
+     quo => $quo);
 
 __END__
 a;b;c;d;e;f
