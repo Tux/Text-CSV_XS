c5da5a93c0ca7091f5c905f62d02d7acac59db88
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2016-11-29 13:32:38 +0100

    keep issue 32. No idea from which repo this issue was

---
+++ sandbox/issue_csv_32.pl
@@ -0,0 +1,16 @@
+#!/pro/bin/perl
+
+use 5.18.2;
+use warnings;
+
+use Data::Peek;
+use Text::CSV;
+
+my $csv = Text::CSV->new ({
+    escape_char		=> "\\",
+    allow_loose_escapes	=> 1,
+    auto_diag		=> 1,
+    });
+
+$csv->parse (q{"error, a\' string"});
+DDumper [ $csv->fields ];
