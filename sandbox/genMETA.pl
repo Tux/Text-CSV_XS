61172971bec90e30aa4738a5e8d201b08e031a2c
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2016-11-30 08:16:46 +0100

    New Encode

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -71,7 +71,7 @@ requires:
     DynaLoader:          0
     IO::Handle:          0
 recommends:
-    Encode:              2.87
+    Encode:              2.88
 configure_requires:
     ExtUtils::MakeMaker: 0
 build_requires:
