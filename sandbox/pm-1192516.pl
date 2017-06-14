731a1ce480e8f326ced5cffcd59101ff29a6f41b
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2017-06-14 14:19:22 +0200

    Update PM example to most recent code

--- sandbox/pm-1192516.pl
+++ sandbox/pm-1192516.pl
@@ -8,25 +8,30 @@ use Text::CSV_XS qw(csv);
 
 my $file = "pm-1192516.csv";
 
-sub make_list_2 {
-    my %opt = @_;
+sub make_list {
+    my %opt  = @_;
 
-    my $headers = $opt{headings};
+    my %plus;
+    my @head = map { s/\+$// && $plus{$_}++; $_ } @{$opt{headings}};
 
     my %foo;
     my $list = csv (
-	in               => $opt{file},
-	headers          => $headers,
-	sep_char         => "|",
-	quote_char       => undef,
-	empty_is_undef   => 1,
-	allow_whitespace => 1,
-	auto_diag        => 1,
-	on_in            => sub {
-	    push @{$foo{$_{item}}}, split m/;\s*/ => $_{"seen in+"} },
-	);
+        in               => $opt{file},
+        headers          => \@head,
+        sep_char         => "|",
+        quote_char       => undef,
+        empty_is_undef   => 1,
+        allow_whitespace => 1,
+        auto_diag        => 1,
+        on_in            => sub {
+            foreach my $f (@head) {
+                $f eq "item" and next;
+                push @{$foo{$_{item}}{$f}}, $plus{$f} ? split m/;\s*/ => $_{$f} : $_{$f};
+                }
+            },
+        );
 
     return \%foo;
     }
 
-DDumper make_list_2 (file => $file, headings => [ "item", "seen in+" ]);
+DDumper make_list (file => $file, headings => [ "item", "seen in+" ]);
