307503e39918ba5cac67fe64122884effec08553
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2018-04-21 13:41:20 +0200

    Guard against missing/removed ppport.h

--- sandbox/genPPPort_h.pl
+++ sandbox/genPPPort_h.pl
@@ -9,26 +9,32 @@ use File::Copy;
 # Check to see if ppport needs updating
 my $ph = "ppport.h";
 
-my ($cv) = (qx{perl $ph --version} =~ m{\b([0-9]\.\w+)});
-if ($Devel::PPPort::VERSION lt $cv) {
-    warn "Your $ph is newer than Devel::PPPort. Update skipped\n";
-    }
-else {
-    my $old = do { local (@ARGV, $/) = ($ph); <> };
-    move $ph, "$ph.bkp";
-
-    Devel::PPPort::WriteFile ("ppport.h");
-
-    my $new = do { local (@ARGV, $/) = ($ph); <> };
-
-    if ($old ne $new) {
-	warn "ppport.h updated to $Devel::PPPort::VERSION\n";
-	unlink "$ph.bkp";
+if (-f $ph) {
+    my ($cv) = (qx{perl $ph --version} =~ m{\b([0-9]\.\w+)});
+    if ($Devel::PPPort::VERSION lt $cv) {
+	warn "Your $ph is newer than Devel::PPPort. Update skipped\n";
 	}
     else {
-	unlink $ph;
-	move "$ph.bkp", $ph;
+	my $old = do { local (@ARGV, $/) = ($ph); <> };
+	move $ph, "$ph.bkp";
+
+	Devel::PPPort::WriteFile ("ppport.h");
+
+	my $new = do { local (@ARGV, $/) = ($ph); <> };
+
+	if ($old ne $new) {
+	    warn "ppport.h updated to $Devel::PPPort::VERSION\n";
+	    unlink "$ph.bkp";
+	    }
+	else {
+	    unlink $ph;
+	    move "$ph.bkp", $ph;
+	    }
 	}
+    }
+else {
+    Devel::PPPort::WriteFile ("ppport.h");
+    warn "Installed new ppport.h $Devel::PPPort::VERSION\n";
     }
 
 my $ppp = qx{perl $ph --compat-version=5.6.1 --quiet CSV_XS.xs};
