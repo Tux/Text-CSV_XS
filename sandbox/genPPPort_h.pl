e1337aa48daae9b2c111ccb2f45565d0b7674eb2
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2019-05-01 11:31:13 +0200

    Update Devel::PPPort checker

--- sandbox/genPPPort_h.pl
+++ sandbox/genPPPort_h.pl
@@ -18,12 +18,12 @@ if (-f $ph) {
 	my $old = do { local (@ARGV, $/) = ($ph); <> };
 	move $ph, "$ph.bkp";
 
-	Devel::PPPort::WriteFile ("ppport.h");
+	Devel::PPPort::WriteFile ($ph);
 
 	my $new = do { local (@ARGV, $/) = ($ph); <> };
 
 	if ($old ne $new) {
-	    warn "ppport.h updated to $Devel::PPPort::VERSION\n";
+	    warn "$ph updated to $Devel::PPPort::VERSION\n";
 	    unlink "$ph.bkp";
 	    }
 	else {
@@ -33,13 +33,25 @@ if (-f $ph) {
 	}
     }
 else {
-    Devel::PPPort::WriteFile ("ppport.h");
-    warn "Installed new ppport.h $Devel::PPPort::VERSION\n";
+    Devel::PPPort::WriteFile ($ph);
+    warn "Installed new $ph $Devel::PPPort::VERSION\n";
     }
 
-my $ppp = qx{perl $ph --compat-version=5.6.1 --quiet CSV_XS.xs};
+my $cv = "5.6.1";
+if (open my $fh, "<", "Makefile.PL") {
+    local $/;
+    if (<$fh> =~ m/^require\s+(5\.[0-9._]+)\b/) {
+	my @v = split m/[._]/ => $1;
+	$v[1] =~ m/^(\d\d\d)(.*)/ and splice @v, 1, 1, $1, $2;
+	$v[2] ||= 0;
+	$cv = join "." => map { $_ + 0 } @v;
+	}
+    }
+
+warn "Checking against minimum perl version $cv\n";
+my $ppp = qx{perl $ph --compat-version=$cv --quiet CSV_XS.xs};
 
 $ppp or exit 0;
 warn "Devel::PPPort suggests the following change:\n--8<---\n",
     $ppp, "-->9---\n",
-    "run 'perl $ph --compat-version=5.6.1 CSV_XS.xs' to see why\n";
+    "run 'perl $ph --compat-version=$cv CSV_XS.xs' to see why\n";
