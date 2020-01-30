0db4b1ef6d0006e37c31811d3ed007d7126d9672
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2020-01-30 08:32:11 +0100

    Test case for issue #19

---
+++ sandbox/issue19.pl
@@ -0,0 +1,32 @@
+#!/pro/bin/perl
+
+use 5.16.2;
+use warnings;
+
+use Text::CSV_XS;
+
+binmode STDOUT, ":encoding(utf-8)";
+binmode STDERR, ":encoding(utf-8)";
+
+my $h = shift // 0;
+
+sub show {
+    say join ", " => map { "\x{231e}$_\x{231d}" } @_;
+    } # show
+
+my $csv = Text::CSV_XS->new ({ auto_diag => 2 });
+if ($h) {
+    say "Calling header";
+    show ($csv->header (*DATA)); # comment out this line to hide the bug
+    }
+while (my $row = $csv->getline (*DATA)) {
+    show (@$row);
+    }
+$csv->error_diag;
+say "EOF: ", $csv->eof ? "Yes" : "No";
+
+__DATA__
+Foo,Bar,Baz
+a,xxx,1
+b,"xx"xx", 2
+c, foo , 3
