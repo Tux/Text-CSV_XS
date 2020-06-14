b822dc2e1d3ac6192d431c29d7e84efae9cf7803
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2020-06-14 11:01:28 +0200

    Missed one place where a subroutine is used without parenthesis #27 (xsawyerx)

--- CSV_XS.pm
+++ CSV_XS.pm
@@ -1272,7 +1272,7 @@ sub _csv_attr {
 
 sub csv {
     @_ && ref $_[0] eq __PACKAGE__ and splice @_, 0, 0, "csv";
-    @_ or croak $csv_usage;
+    @_ or croak ($csv_usage);
 
     my $c = _csv_attr (@_);
 
