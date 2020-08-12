c0b4f1312433acf7ec7d9c8e802d264f7b38c421
H.Merijn Brand - Tux <tux@freedom.nl>
2020-08-12 09:31:02 +0200

    Update to Devel::PPPort-3.60 (3.59 was b0rked)

--- CSV_XS.pm
+++ CSV_XS.pm
@@ -26,7 +26,7 @@ use XSLoader;
 use Carp;
 
 use vars   qw( $VERSION @ISA @EXPORT_OK );
-$VERSION   = "1.44";
+$VERSION   = "1.45";
 @ISA       = qw( Exporter );
 @EXPORT_OK = qw( csv );
 XSLoader::load ("Text::CSV_XS", $VERSION);
