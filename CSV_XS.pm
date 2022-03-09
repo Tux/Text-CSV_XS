1a78aa5dc5fae14fc21dcc12ae8b29e296028285
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2022-03-09 09:38:36 +0100

    Release testing 1.48

--- CSV_XS.pm
+++ CSV_XS.pm
@@ -26,7 +26,7 @@ use XSLoader;
 use Carp;
 
 use vars   qw( $VERSION @ISA @EXPORT_OK );
-$VERSION   = "1.47";
+$VERSION   = "1.48";
 @ISA       = qw( Exporter );
 @EXPORT_OK = qw( csv );
 XSLoader::load ("Text::CSV_XS", $VERSION);
