ef8e2512c31e5ab594e7bdedbf2df5ed495b0aa5
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2023-08-07 13:55:18 +0200

    Versions + Devel::PPPort 3.71

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -77,7 +77,7 @@ recommends:
 configure_requires:
     ExtUtils::MakeMaker: 0
 configure_recommends:
-    ExtUtils::MakeMaker: 7.22
+    ExtUtils::MakeMaker: 7.70
 build_requires:
     Config:              0
 test_requires:
