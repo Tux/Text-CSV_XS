0303a0d0a72a7be0ebb99fb722e450c4514158ee
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2024-06-18 08:13:10 +0200

    Version

--- CSV_XS.pm
+++ CSV_XS.pm
@@ -23,7 +23,7 @@ use XSLoader;
 use Carp;
 
 use vars qw( $VERSION @ISA @EXPORT_OK %EXPORT_TAGS );
-$VERSION = "1.54";
+$VERSION = "1.55";
 @ISA     = qw( Exporter );
 XSLoader::load ("Text::CSV_XS", $VERSION);
 
