52a97ad18de437528f3769a00917efbd61ec075d
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2024-10-28 11:33:42 +0100

    spellcheck

--- CSV_XS.pm
+++ CSV_XS.pm
@@ -4378,7 +4378,7 @@ is identical to
       });
 
 It can be used for ignoring errors as well as for just keeping the error in
-case of analisys after the C<csv ()> function has returned.
+case of analysis after the C<csv ()> function has returned.
 
  my @err;
  my $aoa = csv (in => "bad.csv, on_error => sub { @err = @_ });
